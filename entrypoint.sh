0df2c257f56919053cc0aab35cf0084bf68e0592
asyongasyong123 <asyongasyong123@gmail.com>
2026-06-16 07:55:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-PORT="${PORT:-8080}"
-sed -i "s|listen 8080;|listen ${PORT};|g" /usr/local/openresty/nginx/conf/nginx.conf
-sed -i "s|listen \[::\]:8080;|listen \[::\]:${PORT};|g" /usr/local/openresty/nginx/conf/nginx.conf
-xray run -c /etc/xray.json &
-exec /usr/local/openresty/bin/openresty -g "daemon off;"
+
+echo "Cloud Run PORT: $PORT"
+
+# I-replace ang ${PORT} sa nginx.conf
+export DOLLAR='$'
+envsubst '${PORT},${DOLLAR}' < /usr/local/openresty/nginx/conf/nginx.conf > /tmp/nginx.conf
+mv /tmp/nginx.conf /usr/local/openresty/nginx/conf/nginx.conf
+
+echo "Starting Xray..."
+/usr/local/bin/xray -config /etc/xray.json &
+
+echo "Starting OpenResty..."
+/usr/local/openresty/bin/openresty -g 'daemon off;' &
+
+# Importante: Hulaton kung naay mamatay nga process
+wait -n
+echo "One process exited, shutting down container"
+exit 1
