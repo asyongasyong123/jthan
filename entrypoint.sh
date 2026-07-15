b2cee8880544bdb23d287a9f4c46cab51704dae4
asyongasyong123 <asyongasyong123@gmail.com>
2026-07-15 16:43:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,15 @@ set -e
 
 echo "✅ Starting Xray..."
 xray run -c /etc/xray/config.json &
+XRAY_PID=$!
 
-sleep 6
+sleep 3
 
 echo "✅ Starting Nginx..."
-nginx -g "daemon off;"
+nginx -g "daemon off;" &
+NGINX_PID=$!
+
+# Kung asa man ni ang mohunong, mohunong sad ang tibuok script
+wait -n $XRAY_PID $NGINX_PID
+echo "⚠️ Usa ka serbisyo nahunong, gipatong ang tibuok proseso..."
+kill $XRAY_PID $NGINX_PID
